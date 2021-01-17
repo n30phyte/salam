eeaa03dc6ae340e74f4b11b709b5fe5338ef6bc6
Michael Kwok <michael.kwok00@gmail.com>
2021-01-16 18:43:04 -0700

    Why did I choose to do a mobile app alone.
    App dev sucks, and I suck too.

--- lib/main.dart
+++ lib/main.dart
@@ -14,26 +14,42 @@ class App extends StatelessWidget {
           primarySwatch: Colors.green,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: Home());
+        home: HomePage(title: 'Home'));
   }
 }
 
-class Home extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  final String title;
+
+  HomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              print('Hi');
-            },
-            tooltip: 'Add new',
-            child: Icon(Icons.add),
-            elevation: 2.0),
-        bottomNavigationBar: BottomAppBar(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [IconButton(onPressed: () {}, icon: Icon(Icons.menu))],
-        )));
+      appBar: AppBar(title: Text(widget.title)),
+      endDrawerEnableOpenDragGesture: true,
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          ListTile(title: Text('Recipes'), onTap: () {}),
+          ListTile(title: Text('Ingredients'), onTap: () {}),
+          ListTile(title: Text('Shopping List'), onTap: () {})
+        ],
+      )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Hi');
+          },
+          tooltip: 'Add new',
+          child: Icon(Icons.add),
+          elevation: 2.0),
+    );
   }
 }
